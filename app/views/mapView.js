e15e88c91d7ca90a224b27f91a56ba89da2c0ae7
Aaron Chambers <achambers@gmail.com>
2013-05-18 15:08:54 +0100

    Adding multiple points to the map for each event

--- app/views/mapView.js
+++ app/views/mapView.js
@@ -6,7 +6,21 @@ module.exports = Em.View.extend({
 
   didInsertElement: function() {
   	var map = L.map('map').setView([51.505, -0.09], 13);
-  	L.marker([51.5, -0.09]).addTo(map);
+
+  	// var events = this.get('events');
+  	var events = [
+	  	{location: {lat: 51.505, long: -0.09}},
+	  	{location: {lat: 51.505, long: -0.08}},
+	  	{location: {lat: 51.510, long: -0.08}},
+	  	{location: {lat: 51.500, long: -0.08}},
+	  	{location: {lat: 51.505, long: -0.07}},
+  	];
+
+  	for(var i = 0; i < events.length; i++) {
+  		var event = events[i];
+  		L.marker([event.location.lat, event.location.long]).addTo(map);
+  	}
+
   	L.tileLayer('http://{s}tile.stamen.com/toner/{z}/{x}/{y}.png', {
 	    attribution: 'Ember Hackathon Wooooop',
 	    maxZoom: 18,
