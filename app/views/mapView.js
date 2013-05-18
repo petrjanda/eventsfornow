94e47f18287bbf0723523d6b0631ff41febb74bb
Aaron Chambers <achambers@gmail.com>
2013-05-18 14:58:36 +0100

    We have a basic map

--- app/views/mapView.js
+++ app/views/mapView.js
@@ -1,7 +1,17 @@
 module.exports = Em.View.extend({
   templateName: 'map',
 
+  _update: function() {
+  }.observes('content'),
+
   didInsertElement: function() {
-    console.log(this.get('controller'))
+  	var map = L.map('map').setView([51.505, -0.09], 13);
+  	L.marker([51.5, -0.09]).addTo(map);
+  	L.tileLayer('http://{s}tile.stamen.com/toner/{z}/{x}/{y}.png', {
+	    attribution: 'Ember Hackathon Wooooop',
+	    maxZoom: 18,
+	    subdomains: ["", "a.", "b.", "c.", "d."],
+	    scheme: "xyz"
+	}).addTo(map);
   }
 })
