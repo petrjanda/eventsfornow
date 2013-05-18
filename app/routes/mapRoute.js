5f2094a590706e8dcf1aaa8c9a2b334782bdfd2b
Petr Janda <petrjanda@me.com>
2013-05-18 15:46:05 +0100

    fix

--- app/routes/mapRoute.js
+++ app/routes/mapRoute.js
@@ -3,25 +3,9 @@ App = require('app')
 module.exports = Em.Route.extend({
   setupController: function(controller) {
     $.getJSON('http://juan-air.local:9292/events?search=london', function(data) {
-      var data = JSON.parse({
-        "events": [
-          {
-            "id": 1,
-            "title": "Hacking with Ember!",
-            "address": "3 Plough Yard, Shoreditch, EC2A 3LP, London",
-            "latitude": 51.5227122,
-            "longitude": -0.0790287,
-            "start_time": null,
-            "end_time": null
-          }
-        ]
-      });
-
-      console.log(data)
-
+      controller.set('content', data.events);
     })
 
     controller.set('content', []);
-
   }
 })
